1cd96f494c7bd853478e82d53526e60e7ffe1119
Young Han <han20@umbc.edu>
2019-10-23 03:59:25 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,29 +15,33 @@ end
 def gross_for_director(d)
   total = 0
   index = 0
-
   while index < d[:movies].length do
     total += d[:movies][index][:worldwide_gross]
     index += 1
   end
-
   total
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  director_index = 0
+  array=[]
+  while director_index < source.length do
+    array << source[director_index][:name]
+    director_index+=1
+  end
+  array
+end
+ 
+def total_gross(source)
+  total = 0
+  index = 0
+  while index < directors_totals(source).length do
+    list_directors = list_of_directors(source)[index]
+    total += directors_totals(source)[list_directors]
+    index+=1
+  end
+  total
 end
 
-def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
-end
 
 
